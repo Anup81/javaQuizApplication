716abceb5fb6227018d19616c7338bfbe1652ab9
Anup81 <anupdhakal81@gmail.com>
2022-07-24 17:30:41 +0545

    Quiz Application Setup

---
+++ src/quiz/application/Login.java
@@ -0,0 +1,16 @@
+package quiz.application;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+
+    // As soon as class runs, we want to see frame so constructor created here
+    Login(){
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new Login(); //anonymous Object
+    }
+}
