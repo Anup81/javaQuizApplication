11db3d8bb2fc7630355cddd2c67a8931277969f5
Anup81 <anupdhakal81@gmail.com>
2022-07-24 17:39:40 +0545

    Setting Up Frame using JFrame

--- src/quiz/application/Login.java
+++ src/quiz/application/Login.java
@@ -1,12 +1,17 @@
 package quiz.application;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Login extends JFrame {
 
 
     // As soon as class runs, we want to see frame so constructor created here
     Login(){
+
+        getContentPane().setBackground(Color.WHITE);
+        setSize(1200, 500);
+        setLocation(400, 200);
         setVisible(true);
     }
 
