2ddcdb2cd942ad847502a1416617bf76762c6eb4
Anup81 <anupdhakal81@gmail.com>
2022-07-27 13:12:55 +0545

    Setting Up Score class and Extending JFrame

--- src/quiz/application/Score.java
+++ src/quiz/application/Score.java
@@ -1,12 +1,33 @@
 package quiz.application;
-
-public class Score {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Score extends JFrame implements ActionListener {
 
     Score(String name, int score){
+        setBounds(400, 150, 750, 550);
+        getContentPane().setBackground(Color.WHITE);
 
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/score.png"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 200, 300, 250);
+        add(image);
+
+        JLabel qno = new JLabel();
+        qno.setBounds(100, 450, 50, 30);
+        qno.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(qno);
+
+        setVisible(true);
     }
 
     public static void main(String[] args){
         new Score("User", 0);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
