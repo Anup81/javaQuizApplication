f581631b67ee85c7f3000a0911838ca04daa96ad
Anup81 <anupdhakal81@gmail.com>
2022-07-27 13:29:20 +0545

    Score Section Completed

--- src/quiz/application/Score.java
+++ src/quiz/application/Score.java
@@ -6,18 +6,32 @@ import java.awt.event.ActionListener;
 public class Score extends JFrame implements ActionListener {
 
     Score(String name, int score){
-        setBounds(400, 150, 750, 550);
+        setBounds(600, 150, 750, 550);
         getContentPane().setBackground(Color.WHITE);
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/score.png"));
-        JLabel image = new JLabel(i1);
+        Image i2 = i1.getImage().getScaledInstance(300, 250, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
         image.setBounds(0, 200, 300, 250);
         add(image);
 
-        JLabel qno = new JLabel();
-        qno.setBounds(100, 450, 50, 30);
-        qno.setFont(new Font("Tahoma", Font.PLAIN, 24));
-        add(qno);
+        JLabel heading = new JLabel("Thank You" + name + "for playing Quiz Game!");
+        heading.setBounds(45, 30, 700, 30);
+        heading.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(heading);
+
+        JLabel lscore = new JLabel("Your Score is: " + score);
+        lscore.setBounds(350, 200, 300, 30);
+        lscore.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(lscore);
+
+        JButton submit = new JButton("Play Again");
+        submit.setBounds(380, 270, 120, 30);
+        submit.setBackground(new Color(30, 144, 255));
+        submit.setForeground(Color.WHITE);
+        submit.addActionListener(this);
+        add(submit);
 
         setVisible(true);
     }
@@ -28,6 +42,7 @@ public class Score extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        setVisible(false);
+        new Login();
     }
 }
